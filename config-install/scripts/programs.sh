ee888e009f98a9457de19ddaadb651a5d3d77fa6
trebuhw <114191788+trebuhw@users.noreply.github.com>
2024-12-25 22:19:05 +0100

    up wlogout

--- config-install/scripts/programs.sh
+++ config-install/scripts/programs.sh
@@ -2,7 +2,14 @@
 #
 # Instalacja tylko moich programów po wcześniejszej instalacji CachyOS - Hyprland 
 # Pacman
-sudo pacman -S --noconfirm azote btop cliphist cronie floorp-bin foot gnome-disk-utility gparted grimblast-git grub-customizer htop hyprpicker iptables kitty mako mc mkinitcpio-firmware neovim nwg-look os-prober pamixer pavucontrol polkit-gnome qemu-full qemu-img qt5ct ranger rofi-emoji rsync sane sddm simple-scan slurp starship stow sxiv swappy swaybg swaylock-effects-git swaylock-fancy-git system-config-printer tela-circle-icon-theme-all thunar thunar-archive-plugin thunar-volman timeshift trash-cli virt-install virt-manager virt-viewer vlc waybar wl-clipboard wlogout wofi xfce4-terminal yazi yt-dlp zathura zathura-pdf-mupdf zoxide 
+sudo pacman -S --noconfirm azote btop cliphist cronie floorp-bin foot gnome-disk-utility gparted grimblast-git grub-customizer htop hyprpicker iptables kitty mako mc mkinitcpio-firmware neovim nwg-look os-prober pamixer pavucontrol polkit-gnome qemu-full qemu-img qt5ct ranger rofi-emoji rsync sane sddm simple-scan slurp soundconverter starship stow sxiv swappy swaybg swaylock-effects-git swaylock-fancy-git system-config-printer tela-circle-icon-theme-all thumbler thunar thunar-archive-plugin thunar-volman timeshift trash-cli virt-install virt-manager virt-viewer vlc waybar wl-clipboard wlogout wofi xfce4-terminal yazi yt-dlp zathura zathura-pdf-mupdf zoxide 
 #
 # Paru
+# Instalacja Paru = AUR Helper
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+
+# Instalacja programów
 Paru bibata-cursor-theme brother-dcp1610w brscan4 github-desktop google-chrome hyprland-qtutils-git hyprutils-git lpm moc-pulse ookla-speedtest-bin simple-sddm-theme-git sublime-text-4 waypaper
